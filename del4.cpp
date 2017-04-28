8cd4db91cd8dc80ee10e40d39030052372c95744
Norual <jvmlauron@outlook.com>
2017-04-29 04:07:51 +0800

    Bug Fixes
    HashTable Class Modified

--- del4.cpp
+++ del4.cpp
@@ -6,58 +6,59 @@
 #include<ctime>
 
 using namespace std;
-const int TableSize = 1001;
 
-class HashEntry
-{
-	public:
-	string key;
-	unsigned int value;
-	
-	HashEntry (string key, unsigned int value)
-	{
-		this->key = key;
-		this->value = value;
-	}
-};
+//class HashEntry
+//{
+//	public:
+//	string key;
+//	unsigned int value;
+//	
+//	HashEntry (string key, unsigned int value)
+//	{
+//		this->key = key;
+//		this->value = value;
+//	}
+//};
+
 class HashTable
 {
-	private:
-	HashEntry **Table;
+		private:
+		int HashFunction(string key);
 	
 	public:
-	HashTable()
-	{
-		Table = new HashEntry * [TableSize];
-		for (int i=0; i<TableSize; i++)
-		{
-			Table[i]=NULL;
-		}
-	}
-	int HashFunction(const string & key, int TableSize)
-	{
-		int HashVal=0;
-		for (int i = 0; i < key.size(); i++)
-			HashVal = 37 * HashVal + key[i];
-		return HashVal % TableSize;
-	}
-	void Insert(int key, unsigned int value)
-	{
-		int hash = HashFunction(key,value);
-		int i
-		while (Table[hash] != NULL && Table[hash]->key != key)
-		{
-			//quadratic probing
-			hash = HashFunction (hash + i * i);
-			i = i + 1;
-		}
-		if (Table[hash] !=NULL)
-			delete Table[hash];
-		Table[hash]=new HashEntry(key,value);
-	}
-	//search
-	//remove
+		HashTable();
+		void Insert(string key);
+		
+		const int TableSize = 1001;
+		string Table[];
+};
+
+HashTable::HashTable()	
+{
+	Table[TableSize];
 }
+
+int HashTable::HashFunction(string key)
+{
+	int HashVal=0;
+	for (int i = 0; i < key.size(); i++)
+		HashVal = 37 * HashVal + key[i];
+	return HashVal % TableSize;
+}
+
+void HashTable::Insert(string key)
+{
+	int hash = HashFunction(key);
+	int i = 0;
+	int index = hash;
+	while (Table[index] != "" && Table[index] != key && i != TableSize/2)
+	{
+		//quadratic probing
+		index = hash+(i*i);
+		i = i + 1;
+	}
+}
+
 int main()
 {
 	HashTable hash;
@@ -65,13 +66,12 @@ int main()
 	char choice;
 	clock_t start;
 	double duration;
-	ifstream myfile ("InputFile.txt");
 	while (1)
 	{
 		cout<<"Operations"<<endl;
 		cout<<"1.Add"<<endl;
 		cout<<"2.Add from file"<<endl;
-		cout<<"3.Search"<<Endl;
+		cout<<"3.Search"<<endl;
 		cout<<"4.Del"<<endl;
 		cout<<"5.Exit"<<endl;
 		cin>>choice;
@@ -79,37 +79,39 @@ int main()
 		switch(choice)
 		{
 			case '1':
-			cout<<"key"<<endl;
-			cin>>key;
-			cout<<"value"<<endl;
-			cin>>value;
-			start = std::clock();
-			hash.Insert(key, value);
-			duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-			cout<<"Time Elapsed: "<<duration<<endl;
-			start = 0; //reset start time
-			duration = 0; //reset duration
-			break;
+				cout<<"value"<<endl;
+				cin>>value;
+				start = std::clock();
+				hash.Insert(value);
+				duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+				cout<<"Time Elapsed: "<<duration<<endl;
+				start = 0; //reset start time
+				duration = 0; //reset duration
+				break;
 			
 			case '2':
-			cout<<"value"<<endl;
-			cin>>value;
-			cout<<"key"<<endl;
-			cin>>key;
-			start = std::clock();
-			if (myfile.is_open())
-			{
-				while (getline (myfile,line))
+				cin.ignore();
+				string strLoc;
+				string line;
+				
+				cout<<"Enter file location: "<<endl;
+				getline(cin, strLoc);
+				
+				ifstream myfile (strLoc);
+
+				
+				start = std::clock();
+				while (myfile >> line)
 				{
 					cout<<line<<endl;
-					key = line;
-					root = hash.Insert(key);
+					hash.Insert(line);
 				}
-			duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-			cout<<"Time Elapsed: "<<duration<<endl;
-			start = 0; //reset start time
-			duration = 0; //reset duration
-			break;
+				duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+				
+				cout<<"Time Elapsed: "<<duration<<endl;
+				start = 0; //reset start time
+				duration = 0; //reset duration
+				break;
 		}
 	}
 }
